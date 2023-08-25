ef9d1b8da25aff37be5752d6fcc0aaf919561c25
Safoan143 <142906707+Safoan143@users.noreply.github.com>
2023-08-25 00:53:36 -0700

    Add files via upload

---
+++ class 1/index.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$name = "Shafoun bin Chowdhury";
+$age = 20;
+$f_name = "Murshedul alam chowdhury";
+$m_name = "Taslima bagum";
+$c_name = "Chittagong polytechnic institute";
+$grp_name = "<u>CSE</u>";
+$Courses = "Creative it institute CTG Branch";
+$front = " front end web developer";
+$dev = " Web Development";
+
+// single quotation start.
+echo '<h1><u>Using Single quotation</u></h1> <br/>';
+
+echo ' Hey there friends! I am '. $name .' I am ' . $age . 'years old' . '. I am a student of '. $c_name  .  '. <br/>  My father name is ' . $f_name . '. My mother name is '. $m_name . '. I am also a student of &nbsp; '  . $grp_name . ' Depertment' . '. I have complete my web design course in ' . $Courses . '. <br/>Right now am a ' . $front . '. I want to be a Full Stake web Developer'. ' . So i am now admitted doing ' . $dev .'. <br/> <br/> <br/> <br/> <br/>' ;
+
+
+// single quotation engs.
+
+// double quotation start
+
+
+ echo ' <h1><u>Using Double quotation</u></h1><br/> ';
+
+echo " Hey there friends! I am $name. I am $age years old. I am a student of $c_name. <br/> My Father name is $f_name. My mother name is $m_name. I am also a student of $grp_name. Depertment I have complete my web site design corse in $Courses.<br/> Right Now I am a $front. I want to be a Full Stake Web Developer. so, i am now admitted doing $dev <br/> <br/> <br/> <br/> ";
+
+
+echo " <center> <b>Shafoun Bin Chowdhury.</center> <br/> <center> CIT CH IAPWD 2301.</center> <br/> <center> Class : 01 </center><br/><center> Home work : 01 </center><br/> <center> Date : 24.08.2023</b> </center>"
+
+// double quotation end
+
+?>
